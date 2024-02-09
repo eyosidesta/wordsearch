3c21ab1af63d60040479c0fd7ac1fc93d7ab843c
eyosidesta <mamesmsdesta@gmail.com>
2024-02-09 03:27:11 -0500

    exist method added

--- wordsearch/WordSearch.py
+++ wordsearch/WordSearch.py
@@ -4,4 +4,17 @@ class WordSearch:
         self.visited_twice = False
         self.character_not_existe = False
 
-        
+
+    def exist(self, board, word):
+        m = len(board)
+        word_length = len(word)
+
+        for word_index in range (word_length - 1):
+            for i in range(m):
+                for j in range(len(board[i])):
+                    previous = self.get_word_location(word, word_index, board, i, j)
+                    current = self.get_word_location(word, word_index + 1, i, j)
+                    if(self.check_neighbour(previous, current) or self.visited_twice):
+                        return False
+                    
+        return True
